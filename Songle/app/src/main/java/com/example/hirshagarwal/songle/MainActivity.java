4df38e720782f944aad4213749b5e4de4ec53b63
Hirsh Agarwal <hirsh.agarwal@gmail.com>
2017-09-21 23:24:46 +0100

    Forgot to commit...
    Got the basic functionality in the first few pages. 
    Managed to get the bottom sheet working.
    Basic maps integration.

--- Songle/app/src/main/java/com/example/hirshagarwal/songle/MainActivity.java
+++ Songle/app/src/main/java/com/example/hirshagarwal/songle/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hirshagarwal.songle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Start game method
+    public void gameStart(View view){
+        Intent intent = new Intent(this, GameMapActivity.class);
+        startActivity(intent);
+    }
+
 }
