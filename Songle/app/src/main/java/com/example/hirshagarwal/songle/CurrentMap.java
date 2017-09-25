8acf95fa47123094c0bcb649fa5c749e7df23eff
Hirsh Agarwal <hirsh.agarwal@gmail.com>
2017-09-25 20:46:08 +0100

    Working on network client

--- Songle/app/src/main/java/com/example/hirshagarwal/songle/CurrentMap.java
+++ Songle/app/src/main/java/com/example/hirshagarwal/songle/CurrentMap.java
@@ -2,27 +2,22 @@ package com.example.hirshagarwal.songle;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 
 /**
  * Created by Hirsh Agarwal on 9/25/2017.
  */
 
-public class CurrentMap extends AsyncTask<String, Void, String>{
+public class CurrentMap {
 
     private boolean mapLoaded = false;
+    private String mapUrl = "http://www.inf.ed.ac.uk/teaching/courses/selp/data/songs/01/map5.kml";
+
+    NetworkHandler networkHandler = new NetworkHandler(mapUrl);
 
     public void loadMap(){
         // Load the map
 
-    }
-
-    // Interface Methods
-    protected String doInBackground(String... urls){
-        return "";
-    }
-
-    protected void onPostExecute(){
-        
     }
 
     // Getters and Setters
