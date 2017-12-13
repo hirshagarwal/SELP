18deeccc700b869b54eaef114cd541823d973a3b
Hirsh Agarwal <hirsh.agarwal@gmail.com>
2017-12-13 12:15:46 +0000

    Added the home button in the lose screen.

--- Songle/app/src/main/java/com/example/hirshagarwal/songle/LoseActivity.java
+++ Songle/app/src/main/java/com/example/hirshagarwal/songle/LoseActivity.java
@@ -1,7 +1,9 @@
 package com.example.hirshagarwal.songle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoseActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class LoseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lose);
+    }
+
+    public void returnHome(View view){
+        Intent i = getBaseContext().getPackageManager()
+                .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 }
